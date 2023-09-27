fb0eb22d9c6944614287026a2015f70e63d63637
Auri Munoz <amunozhe@redhat.com>
2023-09-27 13:26:06 +0200

    align with test

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -22,7 +22,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello Quarkus";
+        return "Hello World";
     }
 
     @GET
